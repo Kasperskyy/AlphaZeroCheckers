8a69a0d180cf68d4fee0f04e1c6c90f96d120d88
norbix123456 <83508047+norbix123456@users.noreply.github.com>
2021-05-11 18:48:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,38 @@ from montecarlo.montecarlo import MonteCarlo
 from checkers.game import Game
 import InputBuilder
 import ResNetCheckers
+import MCTS
+##### PETLA GIER
 
 
-game = Game()
-board_size_x = game.board.width  # 4
-board_size_y = game.board.height  # 8
-montecarlo = MonteCarlo(Node(game))
+
+def selfplay(numbgame, Model):
+    for i in range(numbgame):
+
+        game = Game()
+
+        montecarlo = MonteCarlo(Node(game))
+        mcts = MCTS(Model)
+        montecarlo.child_finder = mcts.child_finder
+        while not (game.is_over()):
+            montecarlo.simulate(1600)
+            if(len(game.moves) <20):
+                montecarlo.root_node = montecarlo.make_exploratory_choice()
+            else:
+                montecarlo.root_node = montecarlo.make_choice()
+            game.move(montecarlo.root_node[-1])
+            prob = 
+        game.get_winner()
+
+
+
+
+        board_size_x = game.board.width  # 4
+        board_size_y = game.board.height  # 8
+
 historicalBoards = InputBuilder.HistoricalBoards()
-theModel = ResNetCheckers.build()
+      #  theModel = ResNetCheckers.build()
+
 
 while not (game.is_over()):
     x = InputBuilder.build_board_planes(17, historicalBoards, game)
