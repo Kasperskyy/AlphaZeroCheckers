f0b69db172dc183236a86d213af6fdedeff79558
Kasperskyy <45371177+Kasperskyy@users.noreply.github.com>
2021-05-19 23:14:00 +0200

    Update tempFile.py

--- tempFile.py
+++ tempFile.py
@@ -1,11 +1,32 @@
+import keras.models
 import ResNetCheckers
 import main
 import Training
 import numpy as np
+
+
+#!!!!!  When loading a model instead of making a new one, uncomment the loadfile command
+
+#from scratch
+
 myNetwork = ResNetCheckers.Network()
+#from file
+
+#model = keras.models.load_model('AlphaZeroCheckersModel') ###path of file here
+
 model = myNetwork.getModel()
 #[print(i.shape, i.dtype) for i in model.inputs]
 #[print(o.shape, o.dtype) for o in model.outputs]
-trainingData = main.selfplay(1, model)
-Training.trainNetwork(model, trainingData)
-main.evaluate(model,model,2)
+
+for i in range(1000):
+    trainingData = main.selfplay(200, model) # generate self play data    ##the next 6 lines can be commented to omit evaluation
+    #newModel = myNetwork.build()   #create new model to train
+    #newModel.set_weights(model.get_weights()) #copy weights
+    #Training.trainNetwork(newModel, trainingData) # training loop
+    #isNewNetworkBetter = main.evaluate(model, newModel, 100) #evaluate model
+    #if isNewNetworkBetter == True:
+    #   model = newModel
+    Training.trainNetwork(model, trainingData)
+    if i % 10 == 0:
+        model.save('AlphaZeroCheckersModel') ###path of file here
+        print("model saved!")
