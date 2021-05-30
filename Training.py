b8c5f0c062d32dd3d57090b43542066c1dce92c8
Rudaq <puchaczeknieboraczek@gmail.com>
2021-05-30 18:30:26 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,11 +1,13 @@
 import numpy as np
-import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+
 def trainNetwork(model, trainingData):
-    #x, yPolicy, yValue = list(zip(*trainingData))
     x = [i[0] for i in trainingData]
     x = np.asarray(x)
     yPolicy = [i[1] for i in trainingData]
     yPolicy = np.asarray(yPolicy)
     yValue = [i[2] for i in trainingData]
     yValue = np.asarray(yValue)
-    model.fit(x=x, y=[yPolicy, yValue], batch_size=2048, epochs=50, verbose=1)
+    model.fit(x=x, y=[yPolicy, yValue], batch_size=64, epochs=15, verbose=1)
