d3d45c8ec4ef359d8b6e4d137c7c0df457181b32
Rudaq <puchaczeknieboraczek@gmail.com>
2021-05-30 16:04:33 +0200

    Update tempFile.py

--- tempFile.py
+++ tempFile.py
@@ -1,6 +1,7 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import tensorflow as tf
 import keras.models
 import ResNetCheckers
 import main
@@ -11,36 +12,58 @@ from keras import backend as k
 # !!!!!  When loading a model instead of making a new one, uncomment the loadfile command
 
 # from scratch
-#myNetwork = ResNetCheckers.Network()
-#model = myNetwork.getModel()
-victory_threshold = 0.55
-# from file
-model = keras.models.load_model('AlphaZeroCheckersModel') ###path of file here
-k.set_value(model.optimizer.learning_rate, 0.0001)
+myNetwork = ResNetCheckers.Network()
+# model = myNetwork.getModel()
 
+victory_threshold = 0.51
+
+# from file
+model = keras.models.load_model('AlphaZeroCheckersModel')  ###path of file here
+k.set_value(model.optimizer.learning_rate, 0.0001)
+additionalData = []
+trainingData = []
 # [print(i.shape, i.dtype) for i in model.inputs]
 # [print(o.shape, o.dtype) for o in model.outputs]
-additionalData = []
+with open("TrainingData.txt", "rb") as fp:   # Unpickling
+    additionalData = pickle.load(fp)
+
+
 for i in range(1000):
     print("iteration: ", i)
+
     trainingData = main.selfplay(100, model)  # generate self play data
+
+    with open("TrainingDataADDITIONAL.txt", "wb") as fp:
+        pickle.dump(trainingData, fp)
+
     trainingData = trainingData + additionalData
-    newModel = myNetwork.build()  # create new model to train
-    newModel.set_weights(model.get_weights())  # copy weights
+    #newModel = keras.models.clone_model(model)  # create new model to train
+   # inputShape = (4, 8, 17)
+    #newModel.build((None, inputShape))  # replace 10 with number of variables in input layer
+    #newModel.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=tf.keras.optimizers.Adam(0.0001))
+    #newModel.set_weights(model.get_weights())
+    newModel = keras.models.load_model('AlphaZeroCheckersModel')  ###path of file here
+    k.set_value(newModel.optimizer.learning_rate, 0.0001)
     Training.trainNetwork(newModel, trainingData)  # training loop
+
     # ##the next 6 lines can be commented to omit evaluation
-    isNewNetworkBetter = main.evaluate(model, newModel, 100) #evaluate model
-    #print(main.evaluate(model, model, 10))
+    isNewNetworkBetter = main.evaluate(model, newModel, 50)  # evaluate model #CHANGE TO 50 GIER
+    # print(main.evaluate(model, model, 10))
+
     if isNewNetworkBetter > victory_threshold:
+        print(str(isNewNetworkBetter))
         model = newModel
         print("test passed!")
         additionalData = []
+        model.save('AlphaZeroCheckersModel')  ###path of file here
+        print("model saved!")
     else:
         print("test failed!")
+        print(str(isNewNetworkBetter))
         additionalData = trainingData
         with open("TrainingData.txt", "wb") as fp:
             pickle.dump(trainingData, fp)
-    #Training.trainNetwork(model, trainingData)
+    # Training.trainNetwork(model, trainingData)
     # if i % 10 == 0:
-   # model.save('AlphaZeroCheckersModel')  ###path of file here
-  #  print("model saved!")
+    #model.save('AlphaZeroCheckersModel')  ###path of file here
+    #print("model saved!")
