7c2687b6e21127a9f33cfad9ef36e0e74a9ca39d
Kasperskyy <45371177+Kasperskyy@users.noreply.github.com>
2021-06-01 12:30:22 +0200

    Update gamePlayer.py

--- gamePlayer.py
+++ gamePlayer.py
@@ -1,29 +1,25 @@
 import random
-
 import numpy as np
 from copy import deepcopy
-
 from agent import Agent
 from montecarlo.node import Node
 from montecarlo.montecarlo import MonteCarlo
 from checkers.game import Game
 import InputBuilder
 
-##### PETLA GIER
+
 historicalBoards = None
 game = None
-currInput = None
 
 
 def selfplay(numbgame, model):
-    global game, historicalBoards, currInput
+    global game, historicalBoards
 
     doPrints = False  # set to True to see console
 
     totalData = []
     for i in range(numbgame):
         game = Game()
-
         montecarlo = MonteCarlo(Node(game), model)
         gameData = []
         historicalBoards = InputBuilder.HistoricalBoards()
@@ -37,18 +33,20 @@ def selfplay(numbgame, model):
 
             if doPrints:
                 print("turn " + str(len(game.moves)))
+
             currPlayer = game.whose_turn()
             currInput = InputBuilder.build_board_planes(17, historicalBoards, game, currPlayer)
-            montecarlo.simulate(2, currPlayer)  # lets start with 200 and work our way up #dont put a value less than 2 !
+
+            montecarlo.simulate(100, currPlayer)  # number of simulations per turn. do not put less than 2
 
             probabilities_value = montecarlo.get_probabilities()
             probabilities = InputBuilder.convert_to_output(game.get_possible_moves(), probabilities_value)
 
 
-            if len(game.moves) < 20:
+            if len(game.moves) < 8:  #heuristic method of forcing some exploration
                 montecarlo.root_node = montecarlo.make_exploratory_choice()
             else:
-                montecarlo.root_node = montecarlo.make_choice()
+                montecarlo.root_node = montecarlo.make_choice() #rest of moves are the network playing "optimally"
             if montecarlo.root_node.visits != 0:
                 montecarlo.root_node.visits -= 1
 
@@ -67,15 +65,11 @@ def selfplay(numbgame, model):
 
 def evaluate(bestmodel, challenger, num_games):
     global game, historicalBoards, currInput
-    doPrints = True  # ChangHe to True to print information in console
-
-
-    evaluationThreshold = 0.55  # new model must win 55% of games to be declared the winner
     victoryCounter = 0
-
+    doPrints = True
     for i in range(num_games):
         game = Game()
-        p1 = Agent(2, 1, False)  # change to True to play yourself!
+        p1 = Agent(2, 1, False)
         p2 = Agent(2, 2, False)
         if random.random() < 0.5:  # who goes first
             challengerIndex = 2
@@ -85,40 +79,25 @@ def evaluate(bestmodel, challenger, num_games):
             challengerIndex = 1
             montecarloP1 = MonteCarlo(Node(game), challenger)
             montecarloP2 = MonteCarlo(Node(game), bestmodel)
-         #   montecarlo = MonteCarlo(Node(game), bestmodel, challenger)
-         #   challengerIndex = 2
-        print(challengerIndex)
-
 
         montecarloP1.child_finder = child_finder
         montecarloP2.child_finder = child_finder
         montecarloP1.root_node.player_number = 1
         montecarloP2.root_node.player_number = 2
         while not (game.is_over()):
-            #currInput = InputBuilder.build_board_planes(17, historicalBoards, game)
-
-            if doPrints:
-                if p1.botcategory == 0 and p2.botcategory == 0:
-                    print("turn " + str(len(game.moves)))
-                    print("possible: " + str(game.get_possible_moves()))
             if game.whose_turn() == 1:
                 move = p1.make_move(montecarloP1, montecarloP2, game)
             else:
                 move = p2.make_move(montecarloP2, montecarloP1, game)
-            # if doPrints:
-               # print(move)
             game.move(move)
-        if game.get_winner() == challengerIndex:
-            victoryCounter += 1
-            print("chall dub")
-        elif game.get_winner() == None:
-            print("tie")
-        else:
-            print("loss")
-    #if victoryCounter / num_games >= evaluationThreshold:  # did you win 55% of the games at least?
-        #return True
-    #else:
-     #   return False
+        if doPrints:
+            if game.get_winner() == challengerIndex:
+                victoryCounter += 1
+                print("challenger has won")
+            elif game.get_winner() is None:
+                print("tie")
+            else:
+                print("challenger has lost")
     return victoryCounter / num_games
 
 def evaluateplayer(model, num_games, player1, player2):
@@ -134,55 +113,66 @@ def evaluateplayer(model, num_games, player1, player2):
         game = Game()
         if (player1 == 0 and player2 == 1) or (player1 == 1 and player2 == 0):
             if random.random() < 0.5:
-                p1 = Agent(0, 1,visualizable)
-                p2 = Agent(1, 2,visualizable)
+                p1 = Agent(0, 1, visualizable)
+                p2 = Agent(1, 2, visualizable)
                 if cheer == 0:
                     challengerIndex = 1
+                    print("You are black!")
                 else:
                     challengerIndex = 2
-                print("You are black!")
+                    print("You are white!")
             else:
-                p1 = Agent(1, 1,visualizable)
-                p2 = Agent(0, 2,visualizable)
+                p1 = Agent(1, 1, visualizable)
+                p2 = Agent(0, 2, visualizable)
                 if cheer == 0:
                     challengerIndex = 2
+                    print("You are white!")
                 else:
                     challengerIndex = 1
-                print("You are white!")
+                    print("You are black!")
+
         elif (player1 == 0 and player2 == 2) or (player1 == 2 and player2 == 0):
             if random.random() < 0.5:
-                p1 = Agent(0, 1,visualizable)
-                p2 = Agent(2, 2,visualizable)
+                p1 = Agent(0, 1, visualizable)
+                p2 = Agent(2, 2, visualizable)
                 if cheer == 0:
                     challengerIndex = 1
+                    print("You are black!")
                 else:
                     challengerIndex = 2
-                print("You are black!")
+                    print("You are white!")
+
             else:
-                p1 = Agent(2, 1,visualizable)
-                p2 = Agent(0, 2,visualizable)
+                p1 = Agent(2, 1, visualizable)
+                p2 = Agent(0, 2, visualizable)
                 if cheer == 0:
                     challengerIndex = 2
+                    print("You are white!")
                 else:
                     challengerIndex = 1
-                print("You are white!")
+                    print("You are black!")
+
         elif (player1 == 1 and player2 == 2) or (player1 == 2 and player2 == 1):
             if random.random() < 0.5:
-                p1 = Agent(1, 1,visualizable)
-                p2 = Agent(2, 2,visualizable)
+                p1 = Agent(1, 1, visualizable)
+                p2 = Agent(2, 2, visualizable)
                 if cheer == 1:
                     challengerIndex = 1
+                    print("You are black!")
                 else:
                     challengerIndex = 2
+                    print("You are white!")
             else:
-                p1 = Agent(2, 1,visualizable)
-                p2 = Agent(1, 2,visualizable)
+                p1 = Agent(2, 1, visualizable)
+                p2 = Agent(1, 2, visualizable)
                 if cheer == 1:
                     challengerIndex = 2
+                    print("You are white!")
                 else:
                     challengerIndex = 1
+                    print("You are black!")
         elif (player1 == 0 and player2 == 0):
-            p1 = Agent(0,1,visualizable)
+            p1 = Agent(0,1, visualizable)
             p2 = Agent(0, 2, visualizable)
             challengerIndex = 1
         montecarlo = MonteCarlo(Node(game), model)
@@ -195,8 +185,10 @@ def evaluateplayer(model, num_games, player1, player2):
                     print("turn " + str(len(game.moves)))
             if game.whose_turn() == 1:
                 move = p1.make_move(montecarloActive = montecarlo, game = game, montecarloPassive = None)
+                print("Black to play next move")
             else:
                 move = p2.make_move(montecarloActive = montecarlo, game = game, montecarloPassive = None)
+                print("White to play next move")
             game.move(move)
         if game.get_winner() == challengerIndex:
             victoryCounter += 1
@@ -205,13 +197,7 @@ def evaluateplayer(model, num_games, player1, player2):
 
     return victoryCounter, drawCounter
 
-
-
 def child_finder(node, self, callingPlayer):
-
-    '''
-    currINPUT FOR CURRENT STATE OF THE GAME!
-    '''
 
     if node.historical_boards is None:
         node.historical_boards = InputBuilder.HistoricalBoards()
@@ -224,7 +210,7 @@ def child_finder(node, self, callingPlayer):
         child.state.move(move)
         child.historical_boards = deepcopy(node.historical_boards)
         child.player_number = child.state.whose_turn()
-        child.policy_value = InputBuilder.get_child_policy_value(move, expert_policy_values)  # should return a probability value between 0 and 1
+        child.policy_value = InputBuilder.get_child_policy_value(move, expert_policy_values)
         node.add_child(child)
     if node.parent is not None:
         node.update_win_value(float(win_value), callingPlayer)
