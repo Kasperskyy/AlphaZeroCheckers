8d25bb45b5b47261d538654ef4c95d501dba7616
Kasperskyy <45371177+Kasperskyy@users.noreply.github.com>
2021-12-27 11:49:34 +0100

    More Menus in Main
    added the choice of creating a new model or loading pre-saved one.
    TO DO:
    add choice of just creating data, through playing X games
    training model on all files which have names that start with 'TrainingData' and saving the trained model.
    doing the full loop of alphazero: play x games, train model, (optionally evaluate), save model rinse and repeat. Make sure to ask whether the user wishes to evaluate or not
    ask for hyperparameters: some examples: number of games, whether to evaluate or not, victory threshold, number of MC simulations

--- main.py
+++ main.py
@@ -9,34 +9,53 @@ import Training
 import pickle
 from keras import backend as k
 
-# !!!!!  When loading a model instead of making a new one, uncomment the loadfile command
 
-# from scratch
-#myNetwork = ResNetCheckers.Network()
-#model = myNetwork.getModel()
-#model.save('AlphaZeroCheckersModel')  ###path of file here
-victory_threshold = 0.51
+load = int(input("press 1 to create new model[WARNING THIS WILL OVERWRITE THE MODEL SAVED CURRENTLY] or press any other number to load saved model"))
 
-# from file
 model = keras.models.load_model('AlphaZeroCheckersModel')  ###path of file here
-k.set_value(model.optimizer.learning_rate, 0.0001)
+if model is not None and load == 1:
+    confirm = input("Are you sure you want to overwrite your model? type 'confirm' to confirm")
+if confirm == 'confirm' or model is None:
+    # from scratch
+    myNetwork = ResNetCheckers.Network()
+    model = myNetwork.getModel()
+    model.save('AlphaZeroCheckersModel')  ###path of file here
+
+
+
+
 additionalData = []
 trainingData = []
-# [print(i.shape, i.dtype) for i in model.inputs]
-# [print(o.shape, o.dtype) for o in model.outputs]
+    # with open("TrainingDataAll.txt", "rb") as fp:   # Unpickling
+    #   trainingData = pickle.load(fp)
+# k.set_value(model.optimizer.learning_rate, 0.0001)
+# victory_threshold = 0.51
+#drawC=0
+#winC=0
+#lossC=0
+#for i in trainingData:
+ #   if i[2] == 1:
+  #      winC+=1
+   # elif i[2] == 0:
+    #    drawC +=1
+    #else:
+    #    lossC +=1
 #with open("TrainingData.txt", "rb") as fp:   # Unpickling
-#    additionalData = pickle.load(fp)
+   #additionalData = pickle.load(fp)
+#newAdd = additionalData[:len(additionalData)-86]
+#with open("TrainingData.txt", "wb") as fp:
+   #pickle.dump(newAdd, fp)
+#print(model.summary())
 print("Choose 1 to generate self play and train\n")
 print("Choose 2 to test against other agents\n")
-choopaczups = int(input("Choose mode:\n"))
-if choopaczups == 1:
-    for i in range(1000):
+choice = int(input("Choose mode:\n"))
+if choice == 1:
+    for i in range(1):
         print("iteration: ", i)
-
-        trainingData = gamePlayer.selfplay(1, model)  # generate self play data
-
+        trainingData = gamePlayer.selfplay(2000, model)  # generate self play data
+        #model.save('AlphaZeroCheckersModel')  ###path of file here
         with open("TrainingData.txt", "wb") as fp:
-           pickle.dump(trainingData, fp)
+            pickle.dump(trainingData, fp)
 
         #trainingData = trainingData + additionalData
         #newModel = keras.models.load_model('AlphaZeroCheckersModel')  ###path of file here
@@ -60,10 +79,10 @@ if choopaczups == 1:
         #    additionalData = trainingData
         #    with open("TrainingData.txt", "wb") as fp:
             #    pickle.dump(trainingData, fp)
-        # Training.trainNetwork(model, trainingData)
+        #Training.trainNetwork(model, trainingData)
         # if i % 10 == 0:
-        # model.save('AlphaZeroCheckersModel')  ###path of file here
-        # print("model saved!")
+        #model.save('AlphaZeroCheckersModel')  ###path of file here
+        #print("model saved!")
 else:
     numgame = 100
     print("Players:\n")
@@ -74,4 +93,3 @@ else:
     player2 = int(input("Choose second player:\n"))
     victories, draws = gamePlayer.evaluateplayer(model, numgame, player1, player2)
     print("Victories " + str(victories/numgame) + " Draws " + str(draws/numgame))
-
